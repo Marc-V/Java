b28579c525ea5ea78f368d8c4352ab0cfd11ae12
Marc-V <mvsquez9@gmail.com>
2021-06-10 22:15:16 -0400

    javaFun

---
+++ ImportDemo/ImportDemoTest.java
@@ -0,0 +1,7 @@
+public class PitagorasTest {
+    public static void main(String[] args) {
+        ImportDemo iD = new ImportDemo();
+        String currentDate = iD.getCurrentDate();
+        System.out.println(currentDate);
+    }
+}
